6ccf455768a67af69f38745064dc15e7ed85d1a4
Pedro Zavala <jr.pedro177@gmail.com>
2022-01-29 15:34:06 -0600

    base page desc change

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hello World!
+          Base Zavala Wedding Site. Hi
         </p>
         <a
           className="App-link"
