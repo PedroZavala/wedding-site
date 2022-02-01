f32c00dcfeeb72d53f047f7b998b297e4bfce4ed
Pedro Zavala <jr.pedro177@gmail.com>
2022-02-01 11:58:11 -0600

    rsvp form error handling

--- src/components/welcome/rsvp/rsvp.js
+++ src/components/welcome/rsvp/rsvp.js
@@ -11,18 +11,44 @@ import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
 
 const Rsvp = () => {
+    const inputNameRegex = new RegExp('(.{2,} .{2,})');
+
     const [open, setOpen] = React.useState(false);
     const [submit, setSubmit] = React.useState(false);
+    const [submitDisabled, setSubmitDisabled] = React.useState(true);
+    const [inputNameError, setInputNameError] = React.useState(false);
+    const [inputGuestCountError, setInputGuestCountError] = React.useState(false);
     
-    const handleClickOpen = () => {
+    const handleRsvpClickOpen = () => {
         setOpen(true);
     };
 
-    const handleClose = () => {
+    const handleFormNameInput = (e) => {
+        var result = false;
+        inputNameRegex.test(e.target.value) ? result = false : result = true;
+
+        setInputNameError(result);
+        toggleSubmit(result, inputGuestCountError);
+    };
+
+    const handleFormGuestCountInput = (e) => {
+        var result = false;
+        (e.target.value > 0 && e.target.value <= 15) ? result = false : result = true;
+
+        setInputGuestCountError(result)
+        toggleSubmit(result, inputNameError);
+    };
+
+    // Passing in vars instead of ref becuase of a out of state issue
+    const toggleSubmit = (v1, v2) => {
+        v1 || v2 ? setSubmitDisabled(true) : setSubmitDisabled(false);
+    };
+
+    const handleFormClose = () => {
         setOpen(false);
     };
 
-    const handleSubmit = () => {
+    const handleFormSubmit = () => {
         setOpen(false);
         setSubmit(true);
 
@@ -44,8 +70,8 @@ const Rsvp = () => {
 
     return (
         <div>
-        <RsvpButton variant="contained" onClick={handleClickOpen}>RSVP</RsvpButton>
-        <Dialog open={open} onClose={handleClose}>
+        <RsvpButton variant="contained" onClick={handleRsvpClickOpen}>RSVP</RsvpButton>
+        <Dialog open={open} onClose={handleFormClose}>
             <DialogTitle>RSVP</DialogTitle>
             <DialogContent>
                 <DialogContentText>
@@ -53,29 +79,30 @@ const Rsvp = () => {
                     Please let us know how many total guests (including kids)
                     will be joining our special day.
                 </DialogContentText>
-                <TextField
+                <TextField onChange={(e) => handleFormNameInput(e)}
                     margin="dense"
-                    id="name"
-                    label="Your Name"
+                    label="First and Last Name"
                     type="text"
                     fullWidth
-                    variant="standard"
+                    variant="standard"                    
+                    required                    
+                    error={inputNameError}
                 />
-                <TextField
-                    margin="dense"
-                    id="guests"
+                <TextField onChange={(e) => handleFormGuestCountInput(e)}
+                    margin="dense"                    
                     label="Total Guest Count"
-                    type="number"
-                    pattern="[0-9]"
+                    type="number"                    
                     variant="standard"
+                    required
+                    error={inputGuestCountError}
                 />
             </DialogContent>
             <DialogActions>
-                <Button onClick={handleClose}>Cancel</Button>
-                <Button onClick={handleSubmit}>Submit</Button>
+                <Button onClick={handleFormClose}>Cancel</Button>
+                <Button onClick={handleFormSubmit} disabled={submitDisabled}>Submit</Button>
             </DialogActions>
         </Dialog>
-        <Dialog open={submit} onClose={handleClose}>
+        <Dialog open={submit} onClose={handleFormClose}>
         <Alert severity="success">
             <AlertTitle>Thank You</AlertTitle>
                 We can't wait for you to join us.
