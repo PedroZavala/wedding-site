e909d6af96f0c36a0b82024219b8bbed73cbcbd1
Pedro Zavala <jr.pedro177@gmail.com>
2022-02-01 10:27:06 -0600

    ticker styling updates

--- src/components/ticker/ticker.js
+++ src/components/ticker/ticker.js
@@ -7,17 +7,28 @@ const weddingDate = '2022-09-10T18:00:00'
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
   if (completed) {
     // Render a completed state
-    return <span>It' Time!</span>;
+    return (
+      <div className='ticker-content'>
+        <div className='ticker-text ticker-text-done'><p>It's Time!!</p></div>       
+      </div>
+    );
   } else {
     // Render a countdown
-    return <span style={{fontSize: '20px'}}>{days} {hours} {minutes} {seconds}</span>;
+    return (
+      <div className='ticker-content'>
+        <div className='ticker-text ticker-text-ticking'><p>Days</p><p>{days}</p></div>
+        <div className='ticker-text ticker-text-ticking'><p>Hours</p><p>{hours}</p></div>
+        <div className='ticker-text ticker-text-ticking'><p>Minutes</p><p>{minutes}</p></div>
+        <div className='ticker-text ticker-text-ticking'><p>Seconds</p><p>{seconds}</p></div>        
+      </div>
+    );
   }
 };
 
 const Ticker = () => {
     return (
-        <div className='countdown-root'>
-            <Countdown className='countdown-content' date={weddingDate} renderer={renderer}/>
+        <div className='ticker-root'>
+          <Countdown date={weddingDate} renderer={renderer}/>
         </div>
     );
 }
