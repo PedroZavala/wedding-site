1e6dbded3a44a82419567ba1a116ff75b85e7380
Pedro Zavala <jr.pedro177@gmail.com>
2022-02-01 17:06:42 -0600

    styling

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -6,9 +6,9 @@ function Welcome() {
     return (
         <div className='welcome-root' style={{backgroundImage: 'url(/images/welcome-image.jpeg)'}}>
           <div className='welcome-text-box'>
-              <p className='welcome-text welcome-text-title'>Pedro</p>
-              <p className='welcome-text welcome-text-split'>and</p>
               <p className='welcome-text welcome-text-title'>Ashley</p>
+              <p className='welcome-text welcome-text-split'>and</p>
+              <p className='welcome-text welcome-text-title'>Pedro</p>
               <p className='welcome-text welcome-text-date'>09.10.22</p>
           </div>
           <div className='welcome-rsvp-box'>
