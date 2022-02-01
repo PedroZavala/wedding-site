0c19b5e2ba6afe238e3e377a5d279dc2a5e917ab
Pedro Zavala <jr.pedro177@gmail.com>
2022-02-01 12:23:54 -0600

    adding intro image as static image

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -4,7 +4,7 @@ import Rsvp from './rsvp/rsvp';
 
 function Welcome() {
     return (
-        <div className='welcome-root'>
+        <div className='welcome-root' style={{backgroundImage: 'url(/images/welcome-image.jpeg)'}}>
           <div className='welcome-text-box'>
               <h1 className='welcome-text welcome-text-title'>Zavala Wedding</h1>
               <p className='welcome-text welcome-text-date'>September 10, 2022</p>              
