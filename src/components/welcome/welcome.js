4c44daea936609c593bedf395cee87a513a6a0eb
Pedro Zavala <jr.pedro177@gmail.com>
2022-02-02 16:51:16 -0600

    adding images

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -1,10 +1,12 @@
 import './welcome.css'
 import Ticker from '../ticker/ticker'
 import Rsvp from './rsvp/rsvp';
+import image from '../../assets/images/welcome-image.jpeg'
 
 function Welcome() {
     return (
-        <div className='welcome-root' style={{backgroundImage: 'url(/images/welcome-image.jpeg)'}}>
+        <div className='welcome-root'>
+          <div className="background-wrap" style={{backgroundImage: `url(${image})`}}></div>
           <div className='welcome-text-box'>
               <p className='welcome-text welcome-text-title'>Ashley</p>
               <p className='welcome-text welcome-text-split'>and</p>
