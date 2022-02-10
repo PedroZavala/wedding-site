529e6b1f93aeab62067b8af924ff7ed48d6ea7e7
Pedro Zavala <jr.pedro177@gmail.com>
2022-02-10 13:21:27 -0600

    Adding logo animation to intro

--- src/components/background/backgroundShift.js
+++ src/components/background/backgroundShift.js
@@ -5,6 +5,7 @@ import Slideshow from '../slideshow/slideshow'
 const BackgroundShift = () => {
   const scrollYBreak = 1100;
   const [scrollYPosition, setScrollYPosition] = useState(0);
+  const [logoFadeOut, setLogoFadeOut] = useState(1);
 
   const handleScroll = () => {
       const position = window.pageYOffset;
@@ -18,6 +19,10 @@ const BackgroundShift = () => {
       };
   }, []);
 
+  setTimeout(() => {
+    setLogoFadeOut(-2);
+  }, 2500)
+
   const contentEnable = {
     zIndex: `0`,
     opacity: '100'
@@ -30,14 +35,19 @@ const BackgroundShift = () => {
 
   // Updating background image z index for parallex scrolling effect
   return (
-    <div className='background-img-wrap' style={{zIndex: `-1`}}>
-      <div className='background-img-content'
-        style={scrollYPosition < scrollYBreak ? contentEnable : contentDisable}>
-        <Slideshow/>
+    <div>
+      <div className='background-img-wrap logo-intro-wrap' style={{zIndex: logoFadeOut}}>
+        <img className='logo-intro-img' src='/logo.png'></img>
       </div>
-      <div className='background-img-content'
-        style={scrollYPosition < scrollYBreak ? contentDisable : contentEnable}>
-        <img className='background-img' src='/images/slideshow/img4.jpg'></img>
+      <div className='background-img-wrap' style={{zIndex: `-1`}}>
+        <div className='logo-img-content'
+          style={scrollYPosition < scrollYBreak ? contentEnable : contentDisable}>
+          <Slideshow/>
+        </div>
+        <div className='background-img-content'
+          style={scrollYPosition < scrollYBreak ? contentDisable : contentEnable}>
+          <img className='background-img' src='/images/slideshow/img4.jpg'></img>
+        </div>
       </div>
     </div>
   );
