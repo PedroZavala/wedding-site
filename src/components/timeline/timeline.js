8828fe574fbd7edb901458d723b18fb6d190c975
Pedro Zavala <jr.pedro177@gmail.com>
2022-03-16 18:46:07 -0500

    adding timeline table

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -5,9 +5,30 @@ const Timeline = () => {
 
     return (
         <div className='timeline-root' id='timeline'>
-            <p className='timeline-title gold-underline'>Timeline</p>
+            <p className='timeline-title gold-underline'>Day Of Timeline</p>
             <div className='timeline-content'>
-                <p>hi</p>
+                <table>
+                    <tr>
+                        <td className='table-time-cell'>5:00</td>
+                        <td className='table-descr-cell'>Ceremony</td>
+                    </tr>
+                    <tr>
+                        <td className='table-time-cell'>5:30</td>
+                        <td className='table-descr-cell'>Cocktail</td>
+                    </tr>
+                    <tr>
+                        <td className='table-time-cell'>5:45</td>
+                        <td className='table-descr-cell'>Grand Entrance</td>
+                    </tr>
+                    <tr>
+                        <td className='table-time-cell'>6:15</td>
+                        <td className='table-descr-cell'>Fajitas!</td>
+                    </tr>
+                    <tr>
+                        <td className='table-time-cell'>7:30</td>
+                        <td className='table-descr-cell'>Party Time!</td>
+                    </tr>
+                </table>
             </div>
         </div>
     );
